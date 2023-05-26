bf4fdd7c34afe8d7ab0e9fcb2a3870454b524b6e
huminghe <huminghe@126.com>
2023-05-26 14:23:59 +0800

    new data sql

--- sql/data_new.sql
+++ sql/data_new.sql
@@ -1,7 +1,8 @@
 
 
-select a.distinct_id, a.author_id_str, a.country, a.author_country, a.time from
-(select distinct_id, author_id_str, duration_time, `time` as time, `$country` as country, author_country from events where
+select a.distinct_id, a.author_id_str, a.country, a.author_country, a.time, c.groupId, a.manufacturer, a.height from
+(select distinct_id, author_id_str, duration_time, `time` as time, `$country` as country, author_country,
+`$manufacturer` as manufacturer, `$screen_height` as height from events where
 event = 'click' and click_element = 'CARD' and page_url = 'meet'
 and author_id_str is not null
 and `date` >='2022-01-01') a
@@ -12,11 +13,15 @@ and task_result = 'success' and (order_num = 1 or order_num = 0) and isFirstBuy 
 and `date` >= '2022-01-01'
 group by distinct_id) b
 on a.distinct_id = b.distinct_id
+left outer join
+(select first_id, groupId from users) c
+on a.distinct_id = c.first_id
 where a.time < b.first_time
 
 
-select a.distinct_id, a.author_id_str, a.country, a.author_country, a.time from
-(select distinct_id, author_id_str, duration_time, `time` as time, `$country` as country, author_country from events where
+select a.distinct_id, a.author_id_str, a.country, a.author_country, a.time, c.groupId, a.manufacturer, a.height from
+(select distinct_id, author_id_str, duration_time, `time` as time, `$country` as country, author_country,
+`$manufacturer` as manufacturer, `$screen_height` as height from events where
 event = 'click' and click_name = 'LIKE'
 and author_id_str is not null
 and `date` >='2022-01-01') a
@@ -27,5 +32,8 @@ and task_result = 'success' and (order_num = 1 or order_num = 0) and isFirstBuy 
 and `date` >= '2022-01-01'
 group by distinct_id) b
 on a.distinct_id = b.distinct_id
+left outer join
+(select first_id, groupId from users) c
+on a.distinct_id = c.first_id
 where a.time < b.first_time
 
