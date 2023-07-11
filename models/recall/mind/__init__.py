1eb0dbfad56fb45863ed74e6d0d9f240fd14cbd2
huminghe <huminghe@126.com>
2023-07-11 16:13:19 +0800

    add flask server paid

---
+++ models/recall/mind/__init__.py
@@ -0,0 +1,5 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+# @author: huminghe
+# @date: 2023/7/11
+#
---
+++ models/recall/mind/flask_server_paid.py
@@ -0,0 +1,73 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+# @author: huminghe
+# @date: 2023/4/27
+#
+
+
+from flask import Flask, request
+import os
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import inspect
+import random
+import string
+import json
+import sys
+import predict_paid
+
+app = Flask(__name__)
+server_logs_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logs')
+
+
+def response_return_template(code, message, result=None):
+    if result:
+        return {'status': {'code': code, 'message': message}, 'result': result}
+    else:
+        return {'status': {'code': code, 'message': message}}
+
+
+@app.route("/topfunny/recommend_paid", methods=['POST'])
+def recommend_v2():
+    data = request.json
+    history_list = data['history']
+    country = data['country']
+    brand = data['brand']
+    model = data['model']
+    ads_group = data['ads_group']
+    num = data['num']
+
+    app.logger.info("history: " + str(history_list) + ",   country: " + str(country) + ",   brand: " + str(brand) +
+                    ",   model: " + str(model) + ",   ads_group: " + str(ads_group) + ",   num: " + str(num))
+
+    result = predict_paid.predict_author_result(history_list, country, ads_group, brand, model, num, app.logger)
+    result = [{'authorId': str(x[0]), 'score': str(x[1])} for x in result]
+
+    return_value = response_return_template(200, 'OK', result)
+    return json.dumps(return_value, ensure_ascii=False)
+
+
+@app.route("/topfunny/update_online_cg", methods=['POST'])
+def update_online_cg():
+    data = request.json
+    cg_list = data['cg_list']
+    result = predict_paid.update_online_cg(cg_list)
+    return_value = response_return_template(200, 'OK', result)
+    return json.dumps(return_value, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    os.makedirs(server_logs_dir, exist_ok=True)
+    port = int(sys.argv[1])
+
+    logging.root.setLevel(logging.NOTSET)
+    handler = TimedRotatingFileHandler(os.path.join(server_logs_dir, 'server.log'), when="MIDNIGHT",
+                                       encoding='UTF-8')
+    handler.setLevel(logging.INFO)
+    logging_format = logging.Formatter(
+        '%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(lineno)s - %(message)s')
+    handler.setFormatter(logging_format)
+    app.logger.addHandler(handler)
+    app.logger.info('deploy server started.')
+
+    app.run(host='0.0.0.0', port=port, debug=False)
