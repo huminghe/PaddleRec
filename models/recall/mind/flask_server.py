a8482db37aacdc23fd31ce6534c2f8935d0c8881
huminghe <huminghe@126.com>
2023-08-25 11:34:54 +0800

    log level

--- models/recall/mind/flask_server.py
+++ models/recall/mind/flask_server.py
@@ -54,7 +54,7 @@ if __name__ == '__main__':
     logging.root.setLevel(logging.NOTSET)
     handler = TimedRotatingFileHandler(os.path.join(server_logs_dir, 'server.log'), when="MIDNIGHT",
                                        encoding='UTF-8', backupCount=10)
-    handler.setLevel(logging.INFO)
+    handler.setLevel(logging.NOTSET)
     logging_format = logging.Formatter(
         '%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(lineno)s - %(message)s')
     handler.setFormatter(logging_format)
